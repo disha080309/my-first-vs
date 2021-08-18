1bf53b650139530de92f7d231b2b0d2fdcf7562d
disha080309 <84317794+disha080309@users.noreply.github.com>
2021-08-18 16:10:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function clickme()
+{
+    document.getElementById("lbl1").innerHTML="Disha" + name1;
+console.log(name1.toUpperCase());
+console.log(maxnumber);
+console.log(Math.sqrt(maxnumber));
+}
+var name1="Agrawal";
+var maxnumber=Math.max(15,25);
